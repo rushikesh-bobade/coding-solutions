7440d84853cdc9d185af9111a3d8d3c482d41714
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-18 01:03:23 +0530

    ✅ Java Substring Comparisons | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-06-17

---
+++ hackerrank/medium/java-string-compare/solution.cpp
@@ -0,0 +1,21 @@
+
+
+   public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        for (int i = 1; i <= s.length() - k; i++) {
+            String curr = s.substring(i, i + k);
+
+            if (curr.compareTo(smallest) < 0) {
+                smallest = curr;
+            }
+
+            if (curr.compareTo(largest) > 0) {
+                largest = curr;
+            }
+        }
+
+        return smallest + "\n" + largest;
+    }
+
