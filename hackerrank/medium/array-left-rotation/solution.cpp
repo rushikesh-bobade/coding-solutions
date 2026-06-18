d57119de255dfbcb7e576f6a484412a3babd5c4b
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-18 15:42:07 +0530

    ✅ Left Rotation | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-06-18

---
+++ hackerrank/medium/array-left-rotation/solution.cpp
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+
+    int n = arr.size();
+    List<Integer> result = new ArrayList<>(n);
+
+    // effective rotations
+    d = d % n;
+
+    // add elements from d to end
+    for (int i = d; i < n; i++) {
+        result.add(arr.get(i));
+    }
+
+    // add elements from start to d-1
+    for (int i = 0; i < d; i++) {
+        result.add(arr.get(i));
+    }
+
+    return result;
+}
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> result = Result.rotateLeft(d, arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
