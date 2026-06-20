85d387ec06c1510948f2c9de6a85b20c0ab180f9
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-20 16:28:03 +0530

    ✅ Minimum Moves to Make Array Complementary | Medium | Java | Runtime: 0 ms | Memory: 42.8 MB | 2026-06-20

---
+++ leetcode/medium/1674-minimum-moves-to-make-array-complementary/solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minMoves(int[] nums, int limit) {
+        int n = nums.length;
+        int[] diff = new int[2 * limit + 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+
+            int low = Math.min(a, b) + 1;
+            int high = Math.max(a, b) + limit;
+            int sum = a + b;
+
+            diff[2] += 2;
+            diff[low]--;
+            diff[sum]--;
+            diff[sum + 1]++;
+            diff[high + 1]++;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        int cur = 0;
+
+        for (int s = 2; s <= 2 * limit; s++) {
+            cur += diff[s];
+            ans = Math.min(ans, cur);
+        }
+
+        return ans;
+    }
+}
