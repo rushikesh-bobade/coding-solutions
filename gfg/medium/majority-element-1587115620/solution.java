fc5ca37b084cf77f1fabd2115db98a80ed0e0baa
Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>
2026-06-20 16:33:32 +0530

    ✅ Majority Element | Medium | Java | Runtime: N/A | Memory: N/A | 2026-06-20

---
+++ gfg/medium/majority-element-1587115620/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int majorityElement(int arr[]) {
+        int candidate = -1;
+        int count = 0;
+
+        // Find candidate
+        for (int num : arr) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Verify candidate
+        count = 0;
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        return count > arr.length / 2 ? candidate : -1;
+    }
+}
